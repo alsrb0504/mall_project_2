bc14acbae62bbefe52dccf00bcaaf6d90c4b4480
Tae <alsrb8442@naver.com>
2021-08-08 23:26:02 +0900

    🚧 Front / Implement mobile header layout and classNames

--- front/src/components/header/header.jsx
+++ front/src/components/header/header.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Header = (props) => (
       <header>
-          {/* 모바일 */}
-          <FontAwesomeIcon icon={faBars} className="search" />
-          
-        <h1>사이트 이름</h1>
-        <nav>
-          {/* h1 헤더 nav 숨길 것 */}
-          <h1>헤더 nav</h1> 
-          <ul>
-            <li><a href="/">제품 소개</a></li>
-            <li><a href="/">문의 게시판</a></li>
-            <li>
-              <button type="button">로그인</button>
-            </li>
-          </ul>
-        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4">
+              
+              <div className="header">
+                  {/* 모바일 */}
+                <button className="icon-button">
+                  <FontAwesomeIcon icon={faBars} className="sm-only ic-bar" />
+                </button>
+                
+                <h1 className="header-title" >사이트 이름</h1>
+
+                <nav className="sm-hidden">
+                  {/* h1 헤더 nav 숨길 것 */}
+                  <h1 className="visually-hidden">헤더 nav</h1> 
+                  <ul className="header-nav-list" >
+                    <li className="header-nav-item">
+                      <a href="/">제품 소개</a>
+                    </li>
+                    <li className="header-nav-item">
+                      <a href="/">문의 게시판</a>
+                    </li>
+                    <li className="header-nav-item">
+                      <button className="login-button" type="button">로그인</button>
+                    </li>
+                  </ul>
+                </nav>
+
+                <button className="icon-button">
+                  <FontAwesomeIcon icon={faSearch} className="sm-only ic-search" />
+                </button>
+              </div>
+
+            </div>
+          </div>
+        </div>
       </header>
   );
 
